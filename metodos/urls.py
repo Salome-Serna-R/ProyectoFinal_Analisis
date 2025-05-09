1d092da18e54f34cd53f9544b524812750065da3
Salome-Serna-R <124018168+Salome-Serna-R@users.noreply.github.com>
2025-05-09 17:36:18 -0500

    Creacion archivos html base

--- metodos/urls.py
+++ metodos/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('capitulo1/', views.capitulo1, name='capitulo1'),
+    path('capitulo2/', views.capitulo2, name='capitulo2'),
+    path('capitulo3/', views.capitulo3, name='capitulo3'),
+    path('capitulo4/', views.capitulo4, name='capitulo4'),
 ]
--- metodos/views.py
+++ metodos/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def home(request):
-    return render(request, 'metodos/home.html')
+    return render(request, 'home.html')
+
+def capitulo1(request):
+    return render(request, 'capitulo1.html')
+
+def capitulo2(request):
+    return render(request, 'capitulo2.html')
+
+def capitulo3(request):
+    return render(request, 'capitulo3.html')
+
+def capitulo4(request):
+    return render(request, 'capitulo4.html')
