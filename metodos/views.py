041a2f4ab1d76a45c7bfa6a168e3a0b55c6ee1b4
Salome-Serna-R <124018168+Salome-Serna-R@users.noreply.github.com>
2025-05-11 14:13:57 -0500

    Organizacion base del proyecto

--- metodos/views.py
+++ metodos/views.py
@@ -8,11 +8,3 @@ def home(request):
 def capitulo1(request):
     return render(request, 'capitulo1.html')
 
-def capitulo2(request):
-    return render(request, 'capitulo2.html')
-
-def capitulo3(request):
-    return render(request, 'capitulo3.html')
-
-def capitulo4(request):
-    return render(request, 'capitulo4.html')
