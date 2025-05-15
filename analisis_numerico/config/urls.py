fd008ad5cf2b189513a35d9ae79b3b0fcc2af4ce
Salome-Serna-R <124018168+Salome-Serna-R@users.noreply.github.com>
2025-05-15 09:20:50 -0500

    Revert "Proyecto base"
    This reverts commit 12077780cb7ff825cca5dc1747cec3c78f22cee6.

--- analisis_numerico/config/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path("", include("src.application.home.urls")),
-    path("numerical-methods/", include("src.application.numerical_method.urls")),
-]
--- analisis_numerico/src/application/home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views.index_view import Index
-
-app_name = "home"
-urlpatterns = [
-    path("", Index.as_view(), name="index"),
-]
